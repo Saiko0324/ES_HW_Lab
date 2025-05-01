27b7371f9a979db7884514fa3ca392dbf169cda3
Songhuan <b11902098@ntu.edu.tw>
2025-05-01 16:46:35 +0800

    Lab7

---
+++ ESLAB_P7/ESLAB7/Debug/Core/Src/arm_fir_data.d
@@ -0,0 +1,7 @@
+Core/Src/arm_fir_data.o: ../Core/Src/arm_fir_data.c \
+ ../Drivers/CMSIS/DSP/Include/arm_math.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h
+../Drivers/CMSIS/DSP/Include/arm_math.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
